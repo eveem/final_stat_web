c4b4b77466a09468844c209119cb2bc9fce0e520
eveem <even_narak@hotmail.coml>
2019-05-21 04:16:47 +0700

    Updates

---
+++ precache-manifest.3579f7bcfba338af00f77ef805daa75c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2354f59ca085022ae65b0808917676c4",
+    "url": "/final_stat_web/index.html"
+  },
+  {
+    "revision": "c09f14a0cb0a3449dbf1",
+    "url": "/final_stat_web/static/css/main.0662f4c6.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/final_stat_web/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "c09f14a0cb0a3449dbf1",
+    "url": "/final_stat_web/static/js/main.65884562.chunk.js"
+  },
+  {
+    "revision": "5f0944e1fedd0a64a67a",
+    "url": "/final_stat_web/static/js/runtime~main.dc135ebc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/final_stat_web/precache-manifest.5dba5be17100e9ac66ef41838cc38618.js"
+  "/final_stat_web/precache-manifest.3579f7bcfba338af00f77ef805daa75c.js"
 );
 
 self.addEventListener('message', (event) => {
